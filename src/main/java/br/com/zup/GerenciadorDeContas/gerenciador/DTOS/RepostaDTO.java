43c939f8d79975bc0c6e48348ba66c773f3f3095
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-11-10 16:49:47 -0300

    Criar dtos para conta

--- src/main/java/br/com/zup/GerenciadorDeContas/gerenciador/DTOS/RepostaDTO.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/gerenciador/DTOS/RepostaDTO.java
@@ -1,2 +1,76 @@
-package br.com.zup.GerenciadorDeContas.gerenciador.DTOS;public class RepostaDTO {
+package br.com.zup.GerenciadorDeContas.gerenciador.DTOS;
+
+import br.com.zup.GerenciadorDeContas.gerenciador.Status;
+import br.com.zup.GerenciadorDeContas.gerenciador.Tipo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class RepostaDTO {
+    private int id;
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
+    private LocalDateTime dataDePagamento;
+    private Status status;
+
+    public RepostaDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public LocalDateTime getDataDePagamento() {
+        return dataDePagamento;
+    }
+
+    public void setDataDePagamento(LocalDateTime dataDePagamento) {
+        this.dataDePagamento = dataDePagamento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
--- src/main/java/br/com/zup/GerenciadorDeContas/gerenciador/DTOS/RequisicaoDTO.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/gerenciador/DTOS/RequisicaoDTO.java
@@ -1,2 +1,46 @@
-package br.com.zup.GerenciadorDeContas.gerenciador.DTOS;public class RequisicaoDTO {
+package br.com.zup.GerenciadorDeContas.gerenciador.DTOS;
+
+import br.com.zup.GerenciadorDeContas.gerenciador.Tipo;
+import java.time.LocalDate;
+
+public class RequisicaoDTO {
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
+
+    public RequisicaoDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
 }
